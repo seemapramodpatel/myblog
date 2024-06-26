a68d714419a3b7f777f57916671b0ad2be0b9a79
Seema <seemap232004@gmail.com>
2024-06-27 01:50:26 +0530

    tenth commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -125,12 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 
 MEDIA_URL = 'media/'
 
